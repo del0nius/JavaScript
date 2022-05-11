42dc9add5a8fc2ca09d59abd615d21275c7839d0
del0nius <dp.gaponenko@gmail.com>
2022-05-11 10:04:54 +0300

    update HW_1_JS

--- Gaponenko_HW_1_JS/HW_1_JS.js
+++ Gaponenko_HW_1_JS/HW_1_JS.js
@@ -39,8 +39,7 @@ item_6 = 15;
 // 20. Присвоить переменной item_6_type тип переменной item_6
 item_6_type = typeof(item_6);
 // 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type —— 
-console.log('item_6 == ' + item_6);
-console.log('item_6_type == ' + item_6_type);
+console.log('item_6 == ' + item_6, 'item_6_type == ' + item_6_type);
 // 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
 let item_7 = String(item_6);
 // 23. Создать переменную item_7_type
@@ -48,8 +47,7 @@ let item_7_type
 // 24. Присвоить переменной item_7_type тип переменной item_7
 item_7_type = typeof(item_7)
 // 25. Вывести в консоль тип данных item_7 в виде ——  “item_7 == ”  item_7,  “item_7_type == ”  item_7_type —— 
-console.log('item_7 == ' + item_7);
-console.log('item_7_type == ' + item_7_type);
+console.log('item_7 == ' + item_7, 'item_7_type == ' + item_7_type);
 // 26. Создать переменную “age_1” и присвоить ей значение 10
 // 27. Создать переменную “age_2” и присвоить ей значение 18
 // 28. Создать переменную “age_3” и присвоить ей значение 60
